143d882718c1d5bf85e96211c854dc13f5dd7fa0
Đặng Văn Tuấn <102749812+tuandangvan@users.noreply.github.com>
2023-04-16 05:11:17 +0700

    login

--- FreshFoodApp/app/src/main/java/com/example/freshfoodapp/LoginActivity.java
+++ FreshFoodApp/app/src/main/java/com/example/freshfoodapp/LoginActivity.java
@@ -1,14 +1,118 @@
 package com.example.freshfoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.freshfoodapp.API.APIService;
+import com.example.freshfoodapp.API.RetrofitClient;
+import com.example.freshfoodapp.Models.Category;
+import com.example.freshfoodapp.Models.Login;
+import com.example.freshfoodapp.Models.ResponseObject;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
+
+    GoogleSignInOptions googleSignInOptions;
+    GoogleSignInClient googleSignInClient;
+    ConstraintLayout btn_loginGoogle, btnConfirm;
+
+    EditText username;
+    EditText password;
+
+    APIService apiService;
+
+    ResponseObject responseObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Mapping();
+
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        btn_loginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn();
+            }
+        });
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Login(String.valueOf(username.getText()),String.valueOf(password.getText()));
+            }
+        });
+    }
+
+    void Mapping(){
+        btnConfirm = findViewById(R.id.btn_login_signin);
+
+        //Login with google
+        btn_loginGoogle = findViewById(R.id.btn_login_google);
+        username = findViewById(R.id.et_login_username);
+        password = findViewById(R.id.et_login_password);
+    }
+    void SignIn(){
+        Intent signinInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signinInIntent,1000);
+    }
+
+    void Login(String username, String password){
+        apiService = RetrofitClient.getRetrofit().create(APIService.class);
+        apiService.getAll().enqueue(new Callback<List<Category>>() {
+            @Override
+            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
+                List<Category> categories = response.body();
+                Integer size = categories.size();
+                Log.d("Test",size.toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    void navigateToSecondActivity(){
+        finish();
+        //chuyen man hinh sang main
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        Toast.makeText(getApplicationContext(), "Login success!", Toast.LENGTH_SHORT).show();
     }
 }
