2052564d5decd9f1caec56f3feb9d7ab320e4652
tuandangvan <tuandangvann@gmail.com>
2023-04-17 22:09:36 +0700

    profile, upload avatar UI

---
+++ FreshFoodApp/app/src/main/java/com/example/freshfoodapp/ProfileActivity.java
@@ -0,0 +1,83 @@
+package com.example.freshfoodapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    GoogleSignInOptions googleSignInOptions;
+    GoogleSignInClient googleSignInClient;
+    Button btnLogout;
+    ImageView imgAvatar, btn_back;
+
+    EditText name, email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Mapping();
+        //block edit text
+        name.setEnabled(false);
+        email.setEnabled(false);
+
+
+        //logut with google
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutWithGoogle();
+            }
+        });
+
+        //click avatar
+        imgAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(new Intent(ProfileActivity.this, UploadAvatarActivity.class));
+            }
+        });
+        //click back
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+    void logoutWithGoogle(){
+        googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(ProfileActivity.this,LoginActivity.class));
+            }
+        });
+    }
+    void Mapping(){
+        name = findViewById(R.id.et_profile_name);
+        email = findViewById(R.id.et_proflie_email);
+        btnLogout = findViewById(R.id.btn_profile_logout);
+        imgAvatar = findViewById(R.id.iv_profile_avatar);
+        btn_back = findViewById(R.id.btn_proflie_back);
+    }
+}
--- FreshFoodApp/app/src/main/java/com/example/freshfoodapp/SignUpActivity.java
+++ FreshFoodApp/app/src/main/java/com/example/freshfoodapp/SignUpActivity.java
@@ -1,11 +1,13 @@
 package com.example.freshfoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,26 +15,50 @@ public class SignUpActivity extends AppCompatActivity {
 
     private ImageView btnBackLogin;
     private TextView alreadyAccount;
+    EditText username, name, email, password, confirmPassword;
+    ConstraintLayout btnCreateAccout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        btnBackLogin = findViewById(R.id.btn_back_login);
-        alreadyAccount = findViewById(R.id.tv_login_already);
 
+
+        //button back
         btnBackLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
             }
         });
-
+        //button login
         alreadyAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
             }
         });
+
+        //button create account
+        btnCreateAccout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+    void Mapping(){
+        btnBackLogin = findViewById(R.id.btn_back_login);
+        alreadyAccount = findViewById(R.id.tv_login_already);
+        name = findViewById(R.id.name_input_signup);
+        username = findViewById(R.id.user_input_signup);
+        email = findViewById(R.id.email_input_signup);
+        password = findViewById(R.id.password_input_signup);
+        confirmPassword = findViewById(R.id.confirm_password_input_signup);
+        btnCreateAccout = findViewById(R.id.btnSignUp);
     }
 }
